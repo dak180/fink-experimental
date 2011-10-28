7a19475ec4abf49b147006bed52d05559701042b
David Fang <fangism@users.sourceforge.net>
2011-10-28 18:15:47 +0000

    skip dirs

--- fangism/scripts/link-identical-files.sh
+++ fangism/scripts/link-identical-files.sh
@@ -9,29 +9,34 @@ for f in $srcdir/*
 do
 # if file exists in . (destdir)
 dest=`basename $f`
-if test -f $dest
+if test -d $f
 then
-# destdir is not already linked
-if test ! -L $dest
-then
-if diff -q $f $dest
-then
-# and both files are identical
-# create link
-	echo "SAME: $dest -- linking"
-	ln -s -f $f .
+	echo "dir:  $f"
 else
-# report differences
-	echo "DIFF: $dest"
-fi
-else
-	# already linked
-#	echo "link: $dest"
-	:
-fi
-else
-	# only exists in srcdir
-	echo "NEW:  $dest"
+	if test -f $dest
+	then
+		# destdir is not already linked
+		if test ! -L $dest
+		then
+			if diff -q $f $dest
+			then
+			# and both files are identical
+			# create link
+				echo "SAME: $dest -- linking"
+				ln -s -f $f .
+			else
+			# report differences
+				echo "DIFF: $dest"
+			fi
+		else
+			# already linked
+		#	echo "link: $dest"
+			:
+		fi
+	else
+		# only exists in srcdir
+		echo "NEW:  $dest"
+	fi
 fi
 done
 
